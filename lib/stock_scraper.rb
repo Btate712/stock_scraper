5cd11595f514ef2b408e2e3c14fe3e743cc1a9ec
Robert Tate <btate712@gmail.com>
2019-04-02 11:21:15 +0000

    scraping function added

--- lib/stock_scraper.rb
+++ lib/stock_scraper.rb
@@ -1,6 +1,20 @@
 require "stock_scraper/version"
+require 'nokogiri'
+require 'open-uri'
 
 module StockScraper
   class Error < StandardError; end
-  # Your code goes here...
+
+  puts "Enter stock ticker to lookup:"
+  ticker = gets.strip
+  url = "https://www.wsj.com/search/term.html?KEYWORDS=#{ticker}"
+  html = open(url)
+  doc = Nokogiri::HTML(html)
+  puts "Last Price:  #{doc.css('p.last').text}"
+  puts "Would you like more info? (y/n):"
+  if gets.strip.downcase == "y"
+    puts "High: #{doc.css('p.high').text}"
+    puts "Low: #{doc.css('p.low').text}"
+    puts "Volume: #{doc.css('p.volume')[0].text}"
+  end
 end
